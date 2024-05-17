5b9773d938f700e2c3b2e66d5100ab3455d5a785
Huy Ho <azirkingsand0.12@gmail.com>
2024-05-18 00:53:33 +0700

    18/5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularapp';
+  title: string = 'angularapp';
+  imgLink : string ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYREH-XVJq39CufTPwT-pjfDh5mfI-ZOS80nTN9Fc5sA&s';
+  content : string ='<b>Hello<b> Chào mừng đến với Angular 17'
+
+  isActive: boolean = true;
+  sayHello(){
+   this.isActive = !this.isActive
+  }
 }
